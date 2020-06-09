8d845adab5dd6959c7435ac0ef99db9e752c513a
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-06-08 22:57:06 -0700

    homework

--- main.js
+++ main.js
@@ -2,6 +2,56 @@
  * YOUR CODE HERE! *
  *******************/
 
+ function yell(str) {
+   return str + '!';
+ }
+ const result = yell('Hey');
+ result;
+
+ function getFirstCharacter(str1) {
+   return str1[0];
+ }
+ const name = 'Dallas';
+ const char ='D';
+ const locationOne = name.indexOf(char);
+ const locationOneAgain = name[0];
+ locationOneAgain;
+ 
+
+ function getLastCharacter(str2) {
+  return str2.charAt(str2.length-1);
+ }
+ const result1 = getLastCharacter('Hello how are you?');
+ result1;
+ const message = 'Hello how are you?';
+ const sentenceLength = message.length;
+ sentenceLength;
+
+ function getOneCharacter(str3) {
+   return str3[9 - 1];
+ }
+const result2 = getOneCharacter('Hello world!');
+result2;
+
+function getTwoCharacters(str4) {
+ 
+  }
+
+function makeCapitalized(str5) {
+  return str5.toUpperCase();
+}
+const result4 = makeCapitalized('hey you');
+
+function yellLouder(str6) {
+  return str6.toUpperCase() + '!!!';
+}
+const result5 = yellLouder('i said hey');
+
+function getInitials(name) {
+  return name.split(' ');
+  
+    
+}
 
 
 
